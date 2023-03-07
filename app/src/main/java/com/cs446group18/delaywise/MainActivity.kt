b866124c5ab9815c2438827451fa68f428362367
jenitazhang <jenitazhang@gmail.com>
2023-03-06 23:07:55 -0500

    Finalize Layout and Add Button component

--- app/src/main/java/com/cs446group18/delaywise/MainActivity.kt
+++ app/src/main/java/com/cs446group18/delaywise/MainActivity.kt
@@ -3,14 +3,11 @@ package com.cs446group18.delaywise
 import android.os.Bundle
 import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
-import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import androidx.navigation.ui.setupWithNavController
 import com.cs446group18.delaywise.databinding.ActivityMainBinding
-import com.google.android.material.navigation.NavigationView
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,19 +19,6 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        setSupportActionBar(binding.appBarMain.toolbar)
-
-        val drawerLayout: DrawerLayout = binding.drawerLayout
-        val navView: NavigationView = binding.navView
-        val navController = findNavController(R.id.nav_host_fragment_content_main)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        appBarConfiguration = AppBarConfiguration(setOf(
-                R.id.nav_home, R.id.nav_flightinfo, R.id.nav_savedflights), drawerLayout)
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        navView.setupWithNavController(navController)
-
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
--- app/src/main/java/com/cs446group18/delaywise/ui/components/SearchBox.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/components/SearchBox.kt
@@ -1,19 +1,52 @@
 package com.cs446group18.delaywise.ui.components
 
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role.Companion.Button
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.cs446group18.delaywise.R
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
+data class ListItem(val name: String)
 
-suspend fun mockApi(searchText: String): List<String> {
+@Composable
+fun ListItem(item: ListItem, onSearchInputChange: (String) -> Unit) {
+    Card(
+        modifier = Modifier
+            .heightIn(min = 50.dp)
+            .fillMaxWidth()
+            .clickable { onSearchInputChange(item.name) },
+        shape = RoundedCornerShape(0),
+        colors = CardDefaults.cardColors(containerColor = Color.White)
+    ) {
+        Column(
+            verticalArrangement = Arrangement.Center,
+            modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth()
+                .fillMaxSize(0.1F)
+        ) {
+            Text(
+                item.name,
+                textAlign = TextAlign.Left,
+                modifier = Modifier.absolutePadding(7.dp, 0.dp, 7.dp, 0.dp)
+            )
+        }
+    }
+}
+
+suspend fun mockApi(searchText: String): List<ListItem> {
     delay(500L) // synthetic delay
-    return listOf("$searchText A", "$searchText B", "$searchText C")
+    return listOf(ListItem("AC8838"), ListItem("AC8839"), ListItem("AF4031"), ListItem("DL4980"))
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -23,43 +56,42 @@ fun SearchBox() {
         mutableStateOf("")
     }
     var searchResults by remember {
-        mutableStateOf(listOf<String>())
+        mutableStateOf(listOf<ListItem>())
     }
     val scope = rememberCoroutineScope()
+
     Column {
         TextField(
             value = searchInput,
+            shape = RoundedCornerShape(8.dp),
+            colors = TextFieldDefaults.textFieldColors(
+                focusedIndicatorColor = Color(R.color.main_blue).copy(
+                    alpha = 1F
+                )
+            ),
             onValueChange = {
                 searchInput = it
                 scope.launch {
                     searchResults = mockApi(it)
                 }
             },
-            placeholder = { Text("Search") },
-            modifier = Modifier
-                .fillMaxWidth(),
+            placeholder = { Text("ex. AA5555") },
+            modifier = Modifier.fillMaxWidth()
         )
-        for (result in searchResults) {
-            Card(
-                modifier = Modifier
-                    .heightIn(min = 50.dp)
-                    .fillMaxWidth()
-                    .padding(3.dp),
-            ) {
-                Column(
-                    verticalArrangement = Arrangement.Center,
-                    modifier = Modifier.padding(10.dp)
-                ) {
-                    Text(result)
-                }
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .heightIn(max = 150.dp)
+        ) {
+            items(searchResults) { item ->
+                ListItem(item, onSearchInputChange = {
+                    searchInput = it
+                })
             }
-        }
-        Button(onClick = {}, modifier = Modifier.fillMaxWidth()) {
-            Text("Click me!")
         }
     }
 }
 
 @Preview
 @Composable
-fun Preview() = SearchBox()
+fun PreviewBox() = SearchBox()
