a994f61e4acbc7fba75181eabb9a293232aa5d04
hanz-vora <hanz.vora@gmail.com>
2023-03-07 11:27:45 -0500

    Add Delay Message on flight card

--- app/src/main/java/com/cs446group18/delaywise/ui/components/SavedFlight.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/components/SavedFlight.kt
@@ -7,7 +7,6 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -19,6 +18,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.cs446group18.delaywise.R
 
 @Composable
@@ -40,69 +40,82 @@ fun SavedFlight() {
             Column(
                 modifier = Modifier.padding(15.dp)
             ) {
-                Image(modifier = Modifier.size(40.dp), painter = painterResource(R.drawable.__plane_icon), contentDescription = null)
+                Image(
+                    modifier = Modifier.size(40.dp),
+                    painter = painterResource(R.drawable.__plane_icon),
+                    contentDescription = null
+                )
             }
             Column(
                 modifier = Modifier.padding(15.dp)
             ) {
-                Text(
-                    buildAnnotatedString {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(buildAnnotatedString {
                         withStyle(
                             style = SpanStyle(
                                 fontWeight = FontWeight.W800,
                                 color = Color.Black,
                             )
                         ) {
-                            append("Lufthansa - LH 1810")
+                            append("LH 1810")
                         }
-                    }
-                )
-                Row(
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.Center) {
-                    Text(
-                        modifier = Modifier.padding(end = 5.dp),
-                        text = buildAnnotatedString {
+                    })
+                    Box(modifier = Modifier.fillMaxWidth(), contentAlignment = Alignment.CenterEnd) {
+                        Text(buildAnnotatedString {
                             withStyle(
                                 style = SpanStyle(
-                                    color = Color.Gray
+                                    fontWeight = FontWeight.W500, color = Color(0xffFF9900),
+                                    fontSize = 12.sp
                                 )
                             ) {
-                                append("MUC (Munich)")
+                                append("Likely 1h Delay")
                             }
+                        })
+                    }
+                }
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.Center
+                ) {
+                    Text(modifier = Modifier.padding(end = 5.dp), text = buildAnnotatedString {
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Gray
+                            )
+                        ) {
+                            append("MUC (Munich)")
                         }
-                    )
+                    })
                     Image(
-                        modifier = Modifier.size(20.dp).padding(end = 5.dp),
+                        modifier = Modifier
+                            .size(20.dp)
+                            .padding(end = 5.dp),
                         painter = painterResource(id = R.drawable.__plane_icon),
                         contentDescription = null
                     )
 
-                    Text(
-                        modifier = Modifier.padding(end = 5.dp),
-                        text = buildAnnotatedString {
-                            withStyle(
-                                style = SpanStyle(
-                                    color = Color.Gray
-                                )
-                            ) {
-                                append("BCN (Barcelona)")
-                            }
-                        }
-                    )
-                }
-                Text(
-                    modifier = Modifier.padding(),
-                    text = buildAnnotatedString {
+                    Text(modifier = Modifier.padding(end = 5.dp), text = buildAnnotatedString {
                         withStyle(
                             style = SpanStyle(
-                                color = Color.LightGray
+                                color = Color.Gray
                             )
                         ) {
-                            append("Mon 21 March, 2022")
+                            append("BCN (Barcelona)")
                         }
+                    })
+                }
+                Text(modifier = Modifier.padding(), text = buildAnnotatedString {
+                    withStyle(
+                        style = SpanStyle(
+                            color = Color.LightGray
+                        )
+                    ) {
+                        append("Mon 21 March, 2022")
                     }
-                )
+                })
             }
         }
     }
