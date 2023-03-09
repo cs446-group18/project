77360efb438476fe1bd21cd33c2f0e18c51e5163
Wen Lim <zwlim@uwaterloo.ca>
2023-03-09 16:22:31 -0500

    retrieve element from /flightInfo json object

--- src/main/kotlin/com/example/routes/FlightInfoRoute.kt
+++ src/main/kotlin/com/example/routes/FlightInfoRoute.kt
@@ -7,6 +7,8 @@ import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
 import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
 import kotlinx.serialization.json.JsonObject
 
 val API_KEY_AIRLABS = "b0134401-3dd2-469c-85cf-4974afbf338b"
@@ -20,9 +22,12 @@ fun Route.flightInfo() {
         val jsonObject: JsonObject = json.decodeFromString(JsonObject.serializer(), body)
 
         // Accessing values in the JsonObject
-        val output = jsonObject["response"].toString() // access a property by name
+        val responseArray: JsonArray = jsonObject["response"] as JsonArray
+        val responseJsonObject: JsonObject = responseArray.first() as JsonObject
+        val depTime: JsonElement? = responseJsonObject["dep_time"]
+        println(depTime)
 
-        call.respondText(output)
+        call.respondText(depTime.toString())
     }
     get("/") {
         call.respond(200)
