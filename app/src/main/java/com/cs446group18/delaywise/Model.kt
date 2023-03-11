84e732a061bbe597b52b03b39a3e7d83bfd4e4ec
David McNamee <david@mcnamee.io>
2023-03-11 14:49:15 -0500

    json deserialization

--- app/src/main/java/com/cs446group18/delaywise/Model.kt
+++ app/src/main/java/com/cs446group18/delaywise/Model.kt
@@ -1,10 +1,13 @@
 package com.cs446group18.delaywise
 
+import com.cs446group18.delaywise.models.FlightInfo
 import io.ktor.client.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.plugins.logging.*
 import io.ktor.client.statement.*
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.decodeFromString
 
 val client = HttpClient(CIO) {
     install(Logging)
@@ -12,7 +15,9 @@ val client = HttpClient(CIO) {
 
 object Model {
     suspend fun getFlight() {
-        val response = client.get("http://10.0.2.2:8082/flightInfo/AC741")
-        println(response.bodyAsText())
+        val response = client.get("http://10.0.2.2:8082/flightInfo/AC8838")
+        val bodyText = response.bodyAsText()
+        val parsedJson = Json { ignoreUnknownKeys = true }.decodeFromString<FlightInfo>(bodyText)
+        println(parsedJson)
     }
 }
