ce352029a2c5aee1194faa1209a282291c257d12
Faduma-Ahmed <fm2ahmed@edu.uwaterloo.ca>
2023-03-11 09:04:18 -0500

    migrated from xml to android composer

--- app/src/main/java/com/cs446group18/delaywise/ui/flightinfo/FlightInfoView.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/flightinfo/FlightInfoView.kt
@@ -1,34 +1,73 @@
 package com.cs446group18.delaywise.ui.flightinfo
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.absolutePadding
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cs446group18.delaywise.R
 import com.cs446group18.delaywise.ui.components.BottomBar
+import com.cs446group18.delaywise.ui.components.FlightInfoCard
+import com.cs446group18.delaywise.ui.components.TopBar
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import kotlinx.coroutines.launch
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
+private val appFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.montserrat_extrabold,
+            weight = FontWeight.W900,
+            style = FontStyle.Normal
+        ),
+    )
+)
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
 @Composable
 fun FlightInfoView(
     navigator: DestinationsNavigator
 ) {
-    val scope = rememberCoroutineScope()
     Scaffold(
-        bottomBar = { BottomBar(navigator) },
+        topBar = {
+            TopBar(navigator)
+        },
+        bottomBar = {
+            BottomBar(navigator)
+        },
     ) { contentPadding ->
         Column(modifier = Modifier.padding(contentPadding)) {
-            Text("FlightInfoView")
-            Button(onClick = { scope.launch { } }) {
-                Text("Go to Home")
-            }
+            Text(
+                "Lufthansa 256 (LH256)",
+                fontFamily = appFontFamily,
+                fontSize = 32.sp,
+                modifier = Modifier.absolutePadding(left = 10.dp)
+            )
+            FlightInfoCard(
+                flightInfoData = FlightInfoViewModel.FlightInfo(
+            "MUC",
+            "BCN",
+            "Munich",
+            "Barcelona",
+            "10:55",
+            "12:55",
+            "Mon 21 Mar",
+            "Mon 21 Mar"
+            ))
         }
     }
 }
+
+@Preview
+@Composable
+fun PreviewFlightInfoView() = FlightInfoView(EmptyDestinationsNavigator)
--- app/src/main/java/com/cs446group18/delaywise/ui/flightinfo/FlightInfoViewModel.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/flightinfo/FlightInfoViewModel.kt
@@ -3,8 +3,20 @@ package com.cs446group18.delaywise.ui.flightinfo
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.cs446group18.delaywise.ui.components.DelayType
 
 class FlightInfoViewModel : ViewModel() {
+    data class FlightInfo(
+        val departAirport : String,
+        val arrivalAirport : String,
+        val departCity : String,
+        val arrivalCity : String,
+        val departTime: String,
+        val arrivalTime: String,
+        val departDate : String,
+        val arrivalDate: String,
+
+    )
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is gallery Fragment"
