d60afccf623213e517621cb40a9bfe6fafb0cf0e
David McNamee <david@mcnamee.io>
2023-03-12 00:46:29 -0500

    Configure server to build/run in android studio

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -3,7 +3,7 @@ val kotlin_version: String by project
 val logback_version: String by project
 
 plugins {
-    kotlin("jvm") version "1.8.10"
+    kotlin("jvm")
     id("io.ktor.plugin") version "2.2.4"
     id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
@@ -15,10 +15,6 @@ application {
 
     val isDevelopment: Boolean = project.ext.has("development")
     applicationDefaultJvmArgs = listOf("-Dio.ktor.development=$isDevelopment")
-}
-
-repositories {
-    mavenCentral()
 }
 
 dependencies {
