c5d744580ef2a2aa1fc4942fb9399b6862905da3
jenitazhang <jenitazhang@gmail.com>
2023-03-12 22:44:32 -0400

    WIP searchbar

--- app/src/main/java/com/cs446group18/delaywise/ui/home/HomeView.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/home/HomeView.kt
@@ -9,7 +9,6 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.paint
 import androidx.compose.ui.layout.ContentScale
@@ -20,15 +19,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.cs446group18.delaywise.Model
 import com.cs446group18.delaywise.R
 import com.cs446group18.delaywise.ui.components.*
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.annotation.RootNavGraph
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
+
 
 private val appFontFamily = FontFamily(
     fonts = listOf(
@@ -50,21 +47,21 @@ private val bodyFontFamily = FontFamily(
 )
 
 private val savedFlightsList = mutableListOf(
+    HomeViewModel.Flight("AC8836", "AC", "Likely 30m Delay", DelayType.LIKELY, "RDU(Raleigh)", "YYZ(Toronto)", "Scheduled 2:05pm; Mon 13 March, 2023"),
     HomeViewModel.Flight("AC8839", "AC", "Delayed", DelayType.DELAYED, "RDU(Raleigh)", "YYZ(Toronto)", "Delayed to 5pm; Wed 15 March, 2023"),
     HomeViewModel.Flight("AC834", "AC","Likely 15m Delay", DelayType.LIKELY, "YYZ(Toronto)", "MUC(Munich)", "Scheduled 8:30pm; Wed 15 March, 2023"),
     HomeViewModel.Flight("LH1810", "LH","On-Time", DelayType.ONTIME, "MUC(Munich)", "BCN(Barcelona)", "Scheduled 9am; Mon 16 March, 2023"),
     HomeViewModel.Flight("AC8838", "AC","Likely 1h Delay", DelayType.LIKELY, "YYZ(Toronto)", "RDU(Raleigh)", "Scheduled 2pm; Mon 21 March, 2023"),
 )
-
 @OptIn(ExperimentalMaterial3Api::class)
 @RootNavGraph(start = true)
 @Destination
 @Composable
 fun HomeView(
-    navigator: DestinationsNavigator
+    navigator: DestinationsNavigator,
 ) {
     val focusManager = LocalFocusManager.current
-    val scope = rememberCoroutineScope()
+    
     Scaffold(
         modifier = Modifier.clickable(
             indication = null,
@@ -100,12 +97,12 @@ fun HomeView(
             LazyColumn(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .heightIn(max = 423.dp),
+                    .heightIn(max = 500.dp),
                 verticalArrangement = Arrangement.spacedBy(3.dp),
+                contentPadding = PaddingValues(0.dp,0.dp,0.dp,100.dp)
             ){
                 items(savedFlightsList) {
                         flight -> SavedFlightCard(flightData = flight, navigator =  navigator)
-
                 }
             }
         }
