19c8a4694b903ddd3927a0e357245f47ab57b7c2
hanz-vora <hanz.vora@gmail.com>
2023-03-18 14:01:12 -0400

    test data for graph

--- app/src/main/java/com/cs446group18/delaywise/ui/components/CongestionGraph.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/components/CongestionGraph.kt
@@ -1,5 +1,6 @@
 package com.cs446group18.delaywise.ui.components
 
+import android.graphics.Typeface
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
@@ -8,23 +9,38 @@ import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.patrykandpatrick.vico.compose.axis.horizontal.bottomAxis
+import com.patrykandpatrick.vico.compose.axis.horizontal.topAxis
+import com.patrykandpatrick.vico.compose.axis.vertical.endAxis
 import com.patrykandpatrick.vico.compose.axis.vertical.startAxis
 import com.patrykandpatrick.vico.compose.chart.Chart
 import com.patrykandpatrick.vico.compose.chart.line.lineChart
 import com.patrykandpatrick.vico.compose.component.shape.shader.fromBrush
+import com.patrykandpatrick.vico.compose.component.shapeComponent
+import com.patrykandpatrick.vico.compose.component.textComponent
 import com.patrykandpatrick.vico.compose.style.ChartStyle
 import com.patrykandpatrick.vico.compose.style.ProvideChartStyle
 import com.patrykandpatrick.vico.core.DefaultAlpha
 import com.patrykandpatrick.vico.core.DefaultColors
 import com.patrykandpatrick.vico.core.DefaultDimens
+import com.patrykandpatrick.vico.core.axis.AxisPosition
+import com.patrykandpatrick.vico.core.axis.formatter.AxisValueFormatter
+import com.patrykandpatrick.vico.core.axis.formatter.DecimalFormatAxisValueFormatter
+import com.patrykandpatrick.vico.core.axis.formatter.DefaultAxisValueFormatter
+import com.patrykandpatrick.vico.core.axis.formatter.PercentageFormatAxisValueFormatter
+import com.patrykandpatrick.vico.core.axis.vertical.VerticalAxis
 import com.patrykandpatrick.vico.core.chart.line.LineChart
 import com.patrykandpatrick.vico.core.component.shape.LineComponent
 import com.patrykandpatrick.vico.core.component.shape.Shapes
 import com.patrykandpatrick.vico.core.component.shape.shader.DynamicShaders
+import com.patrykandpatrick.vico.core.dimensions.Dimensions
+import com.patrykandpatrick.vico.core.dimensions.MutableDimensions
+import com.patrykandpatrick.vico.core.entry.entriesOf
 import com.patrykandpatrick.vico.core.entry.entryModelOf
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+import kotlin.random.Random
 
 @Composable
 fun m3ChartStyle(
@@ -76,13 +92,33 @@ fun m3ChartStyle(
 @Composable
 fun CongestionGraph(navigator: DestinationsNavigator) {
     val scope = rememberCoroutineScope()
-    val chartEntryModel = entryModelOf(4f, 12f, 8f, 16f)
+    val randList = List(48) { Random.nextInt(0, 180) }
+    var chartEntryModel = entryModelOf(*randList.toTypedArray())
+
     ProvideChartStyle(m3ChartStyle()) {
         Chart(
             chart = lineChart(),
             model = chartEntryModel,
-            startAxis = startAxis(),
-            bottomAxis = bottomAxis(),
+            startAxis = startAxis(
+                horizontalLabelPosition = VerticalAxis.HorizontalLabelPosition.Inside,
+                titleComponent = textComponent(
+                    padding = MutableDimensions(4F, 2F),
+                    color = Color.DarkGray,
+                    background = shapeComponent(Shapes.pillShape, Color.LightGray),
+                ),
+                guideline = null,
+                title = "Delay (Min)",
+                maxLabelCount = 1
+            ),
+            bottomAxis = bottomAxis(
+                titleComponent = textComponent(
+                    padding = MutableDimensions(4F, 2F),
+                    color = Color.DarkGray,
+                    background = shapeComponent(Shapes.pillShape, Color.LightGray),
+                ),
+                title = "Hour",
+                guideline = null
+            ),
         )
     }
 }
