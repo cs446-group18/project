81e46eda1b5128824d5951715eb2b4124e2eb414
hanz-vora <hanz.vora@gmail.com>
2023-03-20 19:03:35 -0400

    labeled graph component

--- app/src/main/java/com/cs446group18/delaywise/ui/airportinfo/AirportInfoView.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/airportinfo/AirportInfoView.kt
@@ -1,37 +1,18 @@
 package com.cs446group18.delaywise.ui.airportinfo
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.absolutePadding
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
-import com.cs446group18.delaywise.R
 import com.cs446group18.delaywise.ui.components.BottomBar
-import com.cs446group18.delaywise.ui.components.CongestionGraph
+import com.cs446group18.delaywise.ui.components.LabeledCongestionGraph
 import com.cs446group18.delaywise.ui.components.TopBar
 import com.ramcosta.composedestinations.annotation.Destination
 import com.ramcosta.composedestinations.navigation.DestinationsNavigator
 import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
 
-private val appFontFamily = FontFamily(
-    fonts = listOf(
-        Font(
-            resId = R.font.montserrat_extrabold,
-            weight = FontWeight.W900,
-            style = FontStyle.Normal
-        ),
-    )
-)
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Destination
@@ -48,17 +29,11 @@ fun AirportInfoView(
         },
     ) { contentPadding ->
         Column(modifier = Modifier.padding(contentPadding)) {
-            Text(
-                "Airport Info",
-                fontFamily = appFontFamily,
-                fontSize = 40.sp,
-                modifier = Modifier.absolutePadding(left = 10.dp)
-            )
-
-            CongestionGraph(navigator = navigator)
+            LabeledCongestionGraph(navigator)
         }
     }
 }
+
 
 @Preview
 @Composable
---
+++ app/src/main/java/com/cs446group18/delaywise/ui/components/LabeledCongestionGraph.kt
@@ -0,0 +1,80 @@
+package com.cs446group18.delaywise.ui.components
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.layout.layout
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.cs446group18.delaywise.R
+import com.ramcosta.composedestinations.navigation.DestinationsNavigator
+import com.ramcosta.composedestinations.navigation.EmptyDestinationsNavigator
+
+private val appFontFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.montserrat_extrabold,
+            weight = FontWeight.W900,
+            style = FontStyle.Normal
+        ),
+    )
+)
+
+@Composable
+fun LabeledCongestionGraph(navigator: DestinationsNavigator) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Text(
+            "Average Departure Delay",
+            fontFamily = appFontFamily,
+            fontSize = 20.sp,
+            modifier = Modifier
+                .padding(4.dp)
+        )
+        Row(
+            modifier = Modifier
+                .weight(1f)
+                .height(IntrinsicSize.Min),
+            verticalAlignment = Alignment.Top
+        ) {
+            Text(
+                "Minutes Delayed",
+                fontFamily = appFontFamily,
+                fontSize = 15.sp,
+                modifier = Modifier
+                    .rotate(-90f)
+                    .vertical()
+                    .padding(4.dp)
+            )
+            CongestionGraph(navigator = navigator)
+        }
+    }
+}
+
+fun Modifier.vertical() =
+    layout { measurable, constraints ->
+        val placeable = measurable.measure(constraints)
+        layout(placeable.height, placeable.width) {
+            placeable.place(
+                x = -(placeable.width / 2 - placeable.height / 2),
+                y = -(placeable.height / 2 - placeable.width / 2)
+            )
+        }
+    }
+
+@Preview
+@Composable
+fun PreviewLabeledCongestionGraph() = LabeledCongestionGraph(
+    navigator = EmptyDestinationsNavigator
+)
