3f2079e47e0ca01d13648cbba3f8281928322c0c
Wen Lim <zwlim@uwaterloo.ca>
2023-03-22 17:56:37 -0400

    refactor to more testing friendly

--- server/src/main/kotlin/com/cs446group18/server/routes/AirportInfoRoute.kt
+++ server/src/main/kotlin/com/cs446group18/server/routes/AirportInfoRoute.kt
@@ -14,6 +14,8 @@ import java.time.temporal.ChronoUnit
 //Wen's api key
 const val API_KEY_AERO_2 = "lY2EJ3AcgGkFcpjJ5CgKWYrZDGy211A5"
 
+const val HOURS_AGO = 2
+
 fun Route.airportInfo() {
     get("/airport/{airport_code}") {
         println("hit /airport/{airport_code}")
@@ -22,12 +24,15 @@ fun Route.airportInfo() {
         var client = HttpClient()
 
         val now = LocalDateTime.now().truncatedTo(ChronoUnit.HOURS) // current time rounded down to the closest hour
-        val eightHoursAgo = now.minusHours(8) // time 8 hours ago from current time
+        val startTime = now.minusHours(HOURS_AGO.toLong()) // time $HOURS_AGO hours ago from current time
+        println("start is ${startTime.toString()}")
+        println("now is ${now.toString()}")
 
         var httpResponse = client.get("$BASE_URL_AERO/airports/$airportCode/flights/departures"){
             headers {
                 append("x-apikey", API_KEY_AERO_2)
-                parameter("start", eightHoursAgo)
+                parameter("airline", "ACA") //TESTING ONLY REMOVE FOR PROD, reduce size of inputs
+                parameter("start", startTime)
                 parameter("end", now)
             }
         }
@@ -36,8 +41,8 @@ fun Route.airportInfo() {
         //go through each response object in depature and calculate the average delay of each flight. dont count early flights as delay
         //we can read the depature_delay and average it out
 
-        val delaysByHour = IntArray(8) // to keep track of total delay for each hour in the last 8 hours
-        val flightsByHour = IntArray(8) // to keep track of number of flights for each hour in the last 8 hours
+        val delaysByHour = IntArray(HOURS_AGO) // to keep track of total delay for each hour in the last 8 hours
+        val flightsByHour = IntArray(HOURS_AGO) // to keep track of number of flights for each hour in the last 8 hours
 
         val departures = responseObject["departures"]?.jsonArray
 
@@ -60,8 +65,8 @@ fun Route.airportInfo() {
                     } else {
                         Duration.between(scheduledOut, actualOut).toMinutes().toInt()
                     }
-//                    println("delay is $delay mins")
-                    val hourIndex = (Duration.between(eightHoursAgo, actualOut).toHours() % 8).toInt()
+                    println("scheduledOutStr is ${scheduledOutStr.toString()} delay is $delay mins")
+                    val hourIndex = (Duration.between(startTime, actualOut).toHours() % HOURS_AGO).toInt()
                     flightsByHour[hourIndex]++
                     if (delay > 0) {
                         delaysByHour[hourIndex] += delay
@@ -72,15 +77,15 @@ fun Route.airportInfo() {
             }
         }
         println("printing results")
-        for (i in 0..7) {
-            val hourStart = eightHoursAgo.plusHours(i.toLong())
-            val hourEnd = eightHoursAgo.plusHours((i + 1).toLong())
+        for (i in 0 until HOURS_AGO) {
+            val hourStart = startTime.plusHours(i.toLong())
+            val hourEnd = startTime.plusHours((i + 1).toLong())
             val hourDelay = if (flightsByHour[i] > 0) delaysByHour[i] / flightsByHour[i] else 0
             println("${hourStart.format(DateTimeFormatter.ISO_LOCAL_TIME)}-${hourEnd.format(DateTimeFormatter.ISO_LOCAL_TIME)}: $hourDelay minutes")
         }
 
 
         client.close()
-        call.respond(200)
+        call.respond(responseObject)
     }
 }
