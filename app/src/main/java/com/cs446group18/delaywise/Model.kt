79fa30ad5eec6d049104dadcb466018f51470c77
hanz-vora <hanz.vora@gmail.com>
2023-03-25 13:14:41 -0400

    airport info congestion graph back end

--- app/src/main/java/com/cs446group18/delaywise/Model.kt
+++ app/src/main/java/com/cs446group18/delaywise/Model.kt
@@ -2,6 +2,7 @@ package com.cs446group18.delaywise
 
 import com.cs446group18.lib.models.FlightInfo
 import io.ktor.client.*
+import io.ktor.client.call.*
 import io.ktor.client.engine.cio.*
 import io.ktor.client.request.*
 import io.ktor.client.plugins.logging.*
@@ -19,5 +20,15 @@ object Model {
         val bodyText = response.bodyAsText()
         val parsedJson = Json { ignoreUnknownKeys = true }.decodeFromString<FlightInfo>(bodyText)
         return parsedJson
+    }
+
+    suspend fun getAirportDelay(airport: String): List<Int> {
+        val response = client.get("http://10.0.2.2:8082/airportDelay/${airport}")
+        val delays = mutableListOf<Int>()
+        val textResponse = response.bodyAsText()
+        for (item in textResponse) {
+            delays.add(item.toInt())
+        }
+        return delays
     }
 }
