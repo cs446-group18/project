1cb7c8d9d3deb47b538b12f45cba92bd44c12ade
hanz-vora <hanz.vora@gmail.com>
2023-03-26 23:01:51 -0400

    date selector for flight info page

--- app/src/main/java/com/cs446group18/delaywise/ui/flightinfo/FlightInfoView.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/flightinfo/FlightInfoView.kt
@@ -1,16 +1,16 @@
 package com.cs446group18.delaywise.ui.flightinfo
 
+import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Bookmark
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.MonitorHeart
+import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
@@ -87,40 +87,44 @@ fun FlightInfoView(
     }
 }
 
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DateSelector(dates : List<String>) {
-    var expanded by remember {mutableStateOf(false)}
-    var selectedOptionText by remember {mutableStateOf(dates[0])}
-    ExposedDropdownMenuBox(expanded = expanded, onExpandedChange = {expanded = !expanded}) {
-        OutlinedTextField(
-            modifier = Modifier
-                .menuAnchor()
-                .fillMaxWidth(0.15f),
-            readOnly = true,
-            value = selectedOptionText,
-            onValueChange = { },
-            label = { Text("Date") },
-            trailingIcon = {
-                ExposedDropdownMenuDefaults.TrailingIcon(
-                    expanded = expanded
-                )
-            },
-            colors = ExposedDropdownMenuDefaults.textFieldColors()
-        )
-        ExposedDropdownMenu(
+    val contextForToast = LocalContext.current.applicationContext
+
+    // state of the menu
+    var expanded by remember {
+        mutableStateOf(false)
+    }
+
+    Box(
+        contentAlignment = Alignment.Center
+    ) {
+        // clock icon
+        IconButton(onClick = {
+            expanded = true
+        }) {
+            Icon(
+                imageVector = Icons.Default.Schedule,
+                contentDescription = "Open Options"
+            )
+        }
+
+        // drop down menu
+        DropdownMenu(
             expanded = expanded,
             onDismissRequest = {
                 expanded = false
             }
         ) {
-            dates.forEach { selectionOption ->
+            // adding items
+            dates.forEach {itemValue ->
                 DropdownMenuItem(
                     onClick = {
-                        selectedOptionText = selectionOption
+                        Toast.makeText(contextForToast, itemValue, Toast.LENGTH_SHORT)
+                            .show()
                         expanded = false
                     },
-                    text = { Text(selectionOption) }
+                    text = { Text(text = itemValue) }
                 )
             }
         }
