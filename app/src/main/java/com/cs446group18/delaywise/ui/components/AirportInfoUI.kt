b0179dd6801526dc594663f05a31df57682db696
hanz-vora <hanz.vora@gmail.com>
2023-04-02 10:35:17 -0400

    clean airport info file

--- app/src/main/java/com/cs446group18/delaywise/ui/components/AirportInfoUI.kt
+++ app/src/main/java/com/cs446group18/delaywise/ui/components/AirportInfoUI.kt
@@ -2,37 +2,26 @@ package com.cs446group18.delaywise.ui.components
 
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
-import androidx.compose.material3.*
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.cs446group18.delaywise.model.getAirlineName
-import com.cs446group18.delaywise.ui.airportinfo.timeLabelGenerator
 import com.cs446group18.delaywise.ui.styles.BodyText
 import com.cs446group18.delaywise.ui.styles.Heading
-import com.cs446group18.delaywise.util.formatAsDate
-import com.cs446group18.delaywise.util.formatAsTime
-import com.cs446group18.delaywise.util.formatInHoursMinutes
 import com.cs446group18.lib.models.Airport
-import com.cs446group18.lib.models.FlightInfo
-import com.ramcosta.composedestinations.navigation.DestinationsNavigator
-import kotlinx.datetime.*
-import java.util.*
-import kotlin.time.Duration
 
 @Composable
 fun AirportInfoUI(airportInfoData: Airport, timeLabels: List<String>, airportDelay: List<Int>) {
-    Column(modifier = Modifier
-        .padding(vertical = 10.dp)
-        .fillMaxHeight()
-        .verticalScroll(rememberScrollState())) {
+    Column(
+        modifier = Modifier
+            .padding(vertical = 10.dp)
+            .fillMaxHeight()
+            .verticalScroll(rememberScrollState())
+    ) {
 
         FullRow {
             Column(horizontalAlignment = Alignment.CenterHorizontally) {
@@ -52,9 +41,11 @@ fun AirportInfoUI(airportInfoData: Airport, timeLabels: List<String>, airportDel
                     DropdownMenuItem(text = { Text("7 days") }, onClick = { /*TODO*/ })
                 }
             }
-            Row(modifier = Modifier
-                .fillMaxWidth()
-                .padding(5.dp), horizontalArrangement = Arrangement.SpaceAround) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(5.dp), horizontalArrangement = Arrangement.SpaceAround
+            ) {
                 Column(horizontalAlignment = Alignment.CenterHorizontally) {
                     BodyText("43%")
                     BodyText("rate of delay")
